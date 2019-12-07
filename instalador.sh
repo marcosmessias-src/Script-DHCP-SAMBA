698208962edd63467b2b15e03d1fccaf1e2e0586
Marcos Messias <57878494+marcosmessias-src@users.noreply.github.com>
2019-12-06 21:42:41 -0300

    Adicionando mais formatações

--- instalador.sh
+++ instalador.sh
@@ -15,7 +15,7 @@ then
 	echo "Não foi possível fazer update... Verifique a conexão ou o arquivo /etc/apt/sources.list"
 	exit 1
 fi
-echo -e "\033[0;32mUPDATE REALIZADO COM SUCESSO.\033[0m"
+echo -e "\033[0;32m---------- UPDATE REALIZADO COM SUCESSO ----------\033[0m"
 #Fim do update
 
 #Instala o SUDO
@@ -23,26 +23,25 @@ apt-get install -y sudo
 
 #-------------------------------------------------------------
 #Instalação do DHCP
-echo -e "\033[0;32mINSTALANDO O DHCP...\033[0m"
+echo -e "\033[0;32m---------- INSTALANDO O DHCP ----------\033[0m"
 if ! apt-get install -y isc-dhcp-server
 then
 	echo "Ocorreu um erro ao instalar o DHCP"
 	exit 1
 fi
-echo -e "\033[0;32mDHCP INSTALADO COM SUCESSO.\033[0m"
+echo -e "\033[0;32m---------- DHCP INSTALADO COM SUCESSO ----------\033[0m"
 #Fim da instalação do DHCP
 
 #--------------------------------------------------------------------
 #Estes comandos configuram o arquivo /etc/default/isc-dhcp-server para a minha rede LAN
-echo Modificando o arquivo Interfaces
 sed -i 's/INTERFACESv4=""/INTERFACESv4="enp0se8"/g' /etc/default/isc-dhcp-server
-echo -e "\033[0;32mARQUIVO ISC-DHCP-SERVER CONFIGURADO.\033[0m"
+echo -e "\033[0;32m---------- ARQUIVO ISC-DHCP-SERVER CONFIGURADO ----------\033[0m"
 
 #--------------------------------------------------------------------
 #Estes comandos configuram o arquivo dhcpd.conf
 mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.backup
 mv dhcpd.txt /etc/dhcp/dhcpd.conf
-echo -e "\033[0;32mARQUIVO /ETC/DHCP/DHCPD.CONF CONFIGURADO.\033[0m"
+echo -e "\033[0;32m---------- ARQUIVO /ETC/DHCP/DHCPD.CONF CONFIGURADO ----------\033[0m"
 
 #--------------------------------------------------------------------
 #Estes comandos adicionam a configuração LAN ao arquivo /etc/network/interfaces
@@ -55,10 +54,10 @@ echo "address 192.168.100.1" >> /etc/network/interfaces
 echo "netmask 255.255.255.0" >> /etc/network/interfaces
 echo "network 192.168.100.0" >> /etc/network/interfaces
 echo "broadcast 192.168.100.255" >> /etc/network/interfaces
-echo -e "\033[0;32m ARQUIVO INTERFACES CONFIGURADO COM SUCESSO. \033[0m"
+echo -e "\033[0;32m---------- ARQUIVO INTERFACES CONFIGURADO COM SUCESSO ---------- \033[0m"
 #--------------------------------------------------------------------
 #Instalando o SAMBA
-if ! apt-get install -yes samba
+if ! apt-get install -y samba
 then
 	echo "Ocorreu um erro ao tentar instalar o SAMBA"
 	exit 1
@@ -74,6 +73,12 @@ service smbd restart
 
 #---------------------------------------------------------------------
 #Finalizando o script
+echo #################################
+echo #           Criador             #
+echo #       Marcos Messias          #
+echo #  marcosmessias1998@gmail.com  #
+echo #   Github: marcosmessias-src   #
+echo #################################
 echo -e "\033[0;32mPARA ADICIONAR O USUARIO PARA O SAMBA DIGITE: smbpasswd -a nomedousuario\033[0m"
 echo -e "\033[0;32mAGORA ESTÁ TUDO PRONTO  :D\033[0m"
 echo -e "\033[1;32mEXECUTE O COMANDO:\033[0m" "\033[0;31mreboot\033[0m"
