600671b6d789c07d52df885e94614722b17c4aeb
Marcos Messias <57878494+marcosmessias-src@users.noreply.github.com>
2019-12-10 21:09:17 -0300

    adicionando echos no script

--- instalador.sh
+++ instalador.sh
@@ -15,7 +15,7 @@ then
 	echo "Não foi possível fazer update... Verifique a conexão ou o arquivo /etc/apt/sources.list"
 	exit 1
 fi
-echo -e "\033[0;34m---------- UPDATE REALIZADO COM SUCESSO ----------\033[0m"
+echo -e "\033[0;32m---------- UPDATE REALIZADO COM SUCESSO ----------\033[0m"
 #Fim do update
 
 #Instala o SUDO
@@ -23,13 +23,13 @@ apt-get install -y sudo
 
 #-------------------------------------------------------------
 #Instalação do DHCP
-echo -e "\033[0;31m---------- INSTALANDO O DHCP ----------\033[0m"
+echo -e "\033[0;32m---------- INSTALANDO O DHCP ----------\033[0m"
 if ! apt-get install -y isc-dhcp-server
 then
 	echo "Ocorreu um erro ao instalar o DHCP"
 	exit 1
 fi
-echo -e "\033[0;34m---------- DHCP INSTALADO COM SUCESSO ----------\033[0m"
+echo -e "\033[0;32m---------- DHCP INSTALADO COM SUCESSO ----------\033[0m"
 #Fim da instalação do DHCP
 
 #--------------------------------------------------------------------
@@ -41,7 +41,7 @@ echo -e "\033[0;32m---------- ARQUIVO ISC-DHCP-SERVER CONFIGURADO ----------\033
 #Estes comandos configuram o arquivo dhcpd.conf
 mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.backup
 mv dhcpd.txt /etc/dhcp/dhcpd.conf
-echo -e "\033[0;34m---------- ARQUIVO /ETC/DHCP/DHCPD.CONF CONFIGURADO ----------\033[0m"
+echo -e "\033[0;32m---------- ARQUIVO /ETC/DHCP/DHCPD.CONF CONFIGURADO ----------\033[0m"
 
 #--------------------------------------------------------------------
 #Estes comandos adicionam a configuração LAN ao arquivo /etc/network/interfaces
@@ -54,7 +54,7 @@ echo "address 192.168.100.1" >> /etc/network/interfaces
 echo "netmask 255.255.255.0" >> /etc/network/interfaces
 echo "network 192.168.100.0" >> /etc/network/interfaces
 echo "broadcast 192.168.100.255" >> /etc/network/interfaces
-echo -e "\033[0;34m---------- ARQUIVO INTERFACES CONFIGURADO COM SUCESSO ---------- \033[0m"
+echo -e "\033[0;32m---------- ARQUIVO INTERFACES CONFIGURADO COM SUCESSO ---------- \033[0m"
 #--------------------------------------------------------------------
 #Instalando o SAMBA
 if ! apt-get install -y samba
@@ -62,19 +62,26 @@ then
 	echo "Ocorreu um erro ao tentar instalar o SAMBA"
 	exit 1
 fi
-
+echo -e "\033[0;32m---------- SAMBA INSTALADO COM SUCESSO ----------\033[0m"
 #Cria a pasta que será compartilhada no samba
+echo -e "\033[0;32m---------- CRIANDO REPOSITÓRIO SAMBA /root/samba/teste ----------\033[0m"
 mkdir /root/samba/
 mkdir /root/samba/teste
 
+echo -e "\033[0;32m---------- CONFIGURANDO O ARQUIVO SMB.CONF ----------\033[0m"
 mv /etc/samba/smb.conf /etc/samba/smb.original.conf
 mv samba.txt /etc/samba/smb.conf
 
+echo -e "\033[0;32m---------- REINICIANDO O SAMBA ----------\033[0m"
 service smbd restart
+#---------------------------------------------------------------------
+#Reinicia os serviços
+service networking restart
+service isc-dhcp-server restart
 
 #---------------------------------------------------------------------
 #Finalizando o script
 
-echo -e "\033[0;32mPARA ADICIONAR O USUARIO PARA O SAMBA DIGITE: smbpasswd -a nomedousuario\033[0m"
+echo -e "\033[0;33mPARA ADICIONAR O USUARIO PARA O SAMBA DIGITE: smbpasswd -a nomedousuario\033[0m"
 echo -e "\033[0;32mAGORA ESTÁ TUDO PRONTO  :D\033[0m"
-echo -e "\033[1;32mEXECUTE O COMANDO:\033[0m" "\033[0;31mreboot\033[0m"
+
